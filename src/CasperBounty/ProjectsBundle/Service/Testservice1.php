72fa928f05b00b9b2060b0a4884408b50eac7e7a
Егор <yegor1996@bk.ru>
2017-06-27 01:09:22 +0300

    add targets from list

--- src/CasperBounty/ProjectsBundle/Service/Testservice1.php
+++ src/CasperBounty/ProjectsBundle/Service/Testservice1.php
@@ -7,10 +7,20 @@
  */
 
 namespace CasperBounty\ProjectsBundle\Service;
+//use Doctrine\ORM\EntityManager;
+//use Doctrine\ORM\EntityManagerInterface;
+//use Symfony\Bridge\Doctrine
 
+use Doctrine\ORM\EntityManager;
 
 class Testservice1
 {
+    //private $em;
+    public function __construct(EntityManager $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+
+
     public function getHappyMessage()
     {
         $messages = [
@@ -24,4 +34,19 @@ class Testservice1
         return $messages[$index];
     }
 
+    public function addTargetsToProject($projectId,$targetsArr){
+
+        //$this->em->getRepository('');
+        //$em = $this->em->getDoctrine()->getManager();
+
+        //var_dump($this);
+        //$em=$this->entityManager;
+        $project=$this->entityManager->getRepository('CasperBountyProjectsBundle:Projects')->find($projectId);
+        $targets=$this->entityManager->getRepository('CasperBountyTargetsBundle:Targets')->findBy(array('targetid' => $targetsArr));
+
+        foreach ($targets as $target)
+            $project->addTargetid($target);
+
+        $this->entityManager->flush();
+    }
 }
