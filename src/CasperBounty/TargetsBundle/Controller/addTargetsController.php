d3c37a0ddaa43a7a2c8c362815ddcfde6cfc0096
Егор <yegor1996@bk.ru>
2017-12-30 03:16:06 +0300

    Some changes:)

---
+++ src/CasperBounty/TargetsBundle/Controller/addTargetsController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace CasperBounty\TargetsBundle\Controller;
+
+use CasperBounty\TargetsBundle\Entity\Targets;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+
+class addTargetsController extends Controller
+{
+
+    /**
+     * @Route("/projects/{projectId}/addtargets", name="casper_bounty_projects_addTargets")
+     * @Method({"GET"})
+     */
+    public function indexAction($projectId)
+    {
+
+        return $this->render('@CasperBountyTargets/addTargets/addTargetsMain.html.twig',['projectId'=>$projectId]);
+    }
+
+    /**
+     * @Route("/projects/{projectId}/addtargets", name="casper_bounty_targets_addTargets_post")
+     * @Method({"POST"})
+     */
+    public function addTargetsAction($projectId,Request $request)
+    {
+        $targetsText=$request->get('targets');
+        $targetsService = $this->get('casper_bounty_targets.targetsservice');
+        $targetsService ->setProjectId($projectId);
+        $newTargetsCount=$targetsService->addHosts($targetsText);
+
+        return $this->render('@CasperBountyTargets/addTargets/addTargetsMain.html.twig',array('projectId'=>$projectId,'newTargetsCount'=>count($newTargetsCount)));
+    }
+
+    /**
+     * @Route("/projects/{projectId}/addtargetsfromfile", name="casper_bounty_targets_addTargets_fromFile")
+     * @Method({"POST"})
+     */
+    public function addTargetsFromFileAction($projectId,Request $request)
+    {
+        $targetsFiles=$request->files->get('hostsFiles');
+
+        if(!empty($targetsFiles)) {
+            $targetsService = $this->get('casper_bounty_targets.targetsservice');
+            $targetsService->setProjectId($projectId);
+            $newTargetsCount = $targetsService->addHostsFromFile($targetsFiles);
+            //dump($newTargetsCount);
+        }
+
+
+
+        //$targetsService = $this->get('casper_bounty_targets.targetsservice');
+        //$newTargetsCount=$targetsService->addHosts($targetsText,$projectId);
+        die();
+        //return $this->render('@CasperBountyTargets/addTargets/addTargetsMain.html.twig',array('projectId'=>$projectId,'newTargetsCount'=>count($newTargetsCount)));
+    }
+}
